6d87336650e47cc11ac7ee0f3efaa81dcc3d402a
Danny SMc <danny.smc95@gmail.com>
2021-12-13 23:31:58 +0000

    Updated the version.

--- src/module/engine.ts
+++ src/module/engine.ts
@@ -50,7 +50,7 @@ export class Engine {
 		Injector.register('engine.core', this);
 		Injector.register('engine.options', this.options);
 		Registry.set('engine.status', 'main');
-		Registry.set('engine.version', '0.2.9');
+		Registry.set('engine.version', '0.2.10');
 
 		// Check the mode we are running in.
 		const extension = extname(__filename);

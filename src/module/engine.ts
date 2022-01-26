dfb52bb5e2bf4f4de4e7233b700ae41c1a0e70a2
dannysmc95 <danny.smc95@gmail.com>
2022-01-26 11:16:25 +0000

    Updated DI package.

--- src/module/engine.ts
+++ src/module/engine.ts
@@ -50,7 +50,7 @@ export class Engine {
 		Injector.register('turbo.core', this);
 		Injector.register('turbo.options', this.options);
 		Registry.set('turbo.status', 'main');
-		Registry.set('turbo.version', '0.2.21');
+		Registry.set('turbo.version', '0.2 alpha');
 
 		// Check the mode we are running in.
 		const extension = extname(__filename);
